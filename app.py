3f9796f789598312e267b35680436b98f5b3087a
Eric Olson <ericolsonnv@gmail.com>
2017-10-07 00:35:52 -0700

    Made app more generic

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python3.5
-"""This program does stuff
-"""
-
-import ecc
-import menu
-from subprocess import call
+import pymenu
 
 def main():
-    with menu.MenuHandler() as mh:
-        mh.add_window('utilities', 30, 30)
-        mh.add_window('options', 30, 30)
+    with pymenu.Menu() as menu:
+        # Get screen size for new windows
+        screen_size = menu.screen.getmaxyx()
 
-        mh.add_entry('root', '1. Utilities', 'show: utilities')
-        mh.add_entry('root', '2. Options', 'show: options')
-        mh.add_entry('root', '3. Exit', 'py: exit()')
+        # Create two new windows
+        main_window = menu.add_window('Main Window', screen_size[0], screen_size[1])
+        sub_window = menu.add_window('Sub Window', screen_size[0], screen_size[1])
 
-        mh.add_entry('utilities', '1. Do cool function', '')
-        mh.add_entry('utilities', '2. Do other cool function', '')
-        mh.add_entry('utilities', '3. Go back', 'show: root')
+        # Add menu entries to main window
+        main_window.add_entry('Go to Sub Window', 'show: Sub Window')
+        main_window.add_entry('Don\'t do anything', '')
+        main_window.add_entry('Exit', 'py: exit()')
 
-        mh.add_entry('options', '1. Configure some option', '')
-        mh.add_entry('options', '2. Go back', 'show: root')
+        # Add menu entries to sub window
+        sub_window.add_entry('Don\'t go back', '')
+        sub_window.add_entry('Go back', 'show: Main Window')
 
-        mh.show_window('root')
+        # Start program on main window
+        menu.show_window('Main Window')
 
 if __name__ == '__main__':
     main()
